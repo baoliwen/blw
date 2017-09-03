375aed90dd32bac2550e7014833666d0153c9afe
baoliwen <450541705@qq.com>
2017-09-03 14:15:28 +0800

    加入redis缓存

--- blw/blw/blwweb/src/main/java/com/blw/test/controller/TestController.java
+++ blw/blw/blwweb/src/main/java/com/blw/test/controller/TestController.java
@@ -4,6 +4,7 @@ import com.blw.dao.SupplyProductMapper;
 import com.blw.orderdao.TestOrderMapper;
 import com.blw.entity.SupplyProduct;
 import com.blw.entity.TestOrder;
+import com.blw.test.utils.JedisUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -26,6 +27,8 @@ public class TestController {
     @RequestMapping("test")
     @ResponseBody
     public ModelMap testService() {
+        JedisUtils.set("key", "value", 10);
+        System.out.println("缓存数据+:::::::"+JedisUtils.get("key"));
         List<SupplyProduct> list = supplyProductMapper.selectAll();
         List<TestOrder> result = testOrderMapper.selectAll();
         return new ModelMap().addAttribute("asf", "asdfasf");
